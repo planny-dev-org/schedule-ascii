04b8d8cf15d34fb1d1c096561b39f0d4be03bd8e
Julien Dubost <julien.dubost@planny.ch>
2024-10-23 14:17:59 +0200

    move draw block

--- asr.py
+++ asr.py
@@ -8,18 +8,8 @@ from schedule_ascii.drawer import Drawer
 logging.basicConfig()
 
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        prog="asr.py",
-        description="Load JSON file to an sqlite database, display schedule & metrics,\n"
-        "store db file at the root of JSON file path\n",
-    )
-    parser.add_argument("json_file_path", help="file to run (.mps)")
-
-    parsed_args = parser.parse_args()
-
-    json_parser = JSONParser(parsed_args.json_file_path)
+def do_draw(json_file_path):
+    json_parser = JSONParser(json_file_path)
     rel_path_name = os.path.basename(parsed_args.json_file_path)[:-5] + ".sqlite"
     db_filename = os.path.join(
         os.path.dirname(parsed_args.json_file_path), rel_path_name
@@ -35,3 +25,17 @@ if __name__ == "__main__":
         print(f"{shift[0]} {shift[1]}")
 
     drawer.draw()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog="asr.py",
+        description="Load JSON file to an sqlite database, display schedule & metrics,\n"
+        "store db file at the root of JSON file path\n",
+    )
+    parser.add_argument("json_file_path", help="file to run (.mps)")
+
+    parsed_args = parser.parse_args()
+
+    do_draw(parsed_args.json_file_path)
